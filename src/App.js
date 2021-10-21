e80a5201ea8f2eb28379010d064a1deb2dd712d4
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-10-21 14:51:12 +0600

    Practice Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+
+  const handleAddUser = e => {
+    e.preventDefault();
+
+    const name = nameRef.current.value;
+    const newUser = {
+      name: name
+    }
+
+    fetch("http://localhost:4000/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const addedUser = data;
+        const newUsers = [...users, addedUser];
+        setUsers(newUsers);
+      });
+
+    nameRef.current.value = "";
+
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{users.length}</h2>
+
+      {users.map(user => <li>{user.name}</li>)}
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" ref={nameRef} placeholder="Name" />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
